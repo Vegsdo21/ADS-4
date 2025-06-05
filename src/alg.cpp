5ac0e0b55ae67f492c779d15052468904c9c2bb5
Vegsdo21 <sgt.pepper21@mail.ru>
2025-06-05 08:55:49 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,23 @@
 // Copyright 2021 NNTU-CS
-int countPairs1(int *arr, int len, int value) {
-  return 0;
+int countPairs1(int *numbers, int size, int targetSum)
+{
+  int matchCount = 0;
+  for (int outer = 0; outer < size; ++outer)
+  {
+    for (int inner = outer + 1; inner < size; ++inner)
+    {
+      int currentSum = numbers[outer] + numbers[inner];
+      if (currentSum == targetSum)
+      {
+        matchCount++;
+      }
+      else if (currentSum > targetSum)
+      {
+        break;
+      }
+    }
+  }
+  return matchCount;
 }
 int countPairs2(int *arr, int len, int value) {
   return 0;
